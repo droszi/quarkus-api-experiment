60c39f7061a4f4d5cc73de345118c60122897e47
Attila Dröszler <attila.droszler@emarsys.com>
2023-10-19 19:41:39 +0200

    use mockito

--- src/test/java/com/droszi/api/controller/PushMessageControllerTest.java
+++ src/test/java/com/droszi/api/controller/PushMessageControllerTest.java
@@ -1,39 +1,30 @@
 package com.droszi.api.controller;
 
-import static com.droszi.api.db.public_.Tables.MESSAGES;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.droszi.api.model.PushMessageModel;
-import com.droszi.api.services.JooqService;
-
+import com.droszi.api.repository.PushMessageRepository;
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.vertx.core.json.Json;
-import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response.Status;
-import org.jooq.impl.DSL;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
-@Transactional
 public class PushMessageControllerTest {
-  @Inject
-  JooqService jooqService;
-
-  @BeforeEach
-  public void prepareDB() {
-    jooqService.getDbContext().deleteFrom(MESSAGES).execute();
-  }
+  @InjectMock
+  PushMessageRepository repository;
 
   @Test
   @TestHTTPEndpoint(PushMessageController.class)
   public void testPost() {
     PushMessageModel pushMessage = new PushMessageModel(1, "msg", "ttl");
+
     given()
         .contentType(MediaType.APPLICATION_JSON)
         .body(pushMessage)
@@ -44,14 +35,6 @@ public class PushMessageControllerTest {
         .contentType(MediaType.APPLICATION_JSON)
         .body(is(Json.encode(pushMessage)));
 
-    int count = jooqService.getDbContext()
-        .fetchCount(
-            DSL.selectFrom(MESSAGES)
-            .where(MESSAGES.USER_ID.eq(Integer.valueOf(1)))
-            .and(MESSAGES.TITLE.eq("ttl"))
-            .and(MESSAGES.MESSAGE.eq("msg"))
-        );
-
-    assertEquals(1, count);
+    verify(repository, times(1)).insert(pushMessage);
   }
 }
